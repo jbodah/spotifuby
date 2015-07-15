209bf4ec971dc14ef0b11f123e8a4ee639b4d497
Josh Bodah <jb3689@yahoo.com>
2015-07-14 20:40:41 -0400

    add RSpotify support

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,9 +1,14 @@
+require 'rspotify'
+require 'yaml'
+
 module Spotify
   extend self
 
-  def play(track = nil)
-    if track.nil?
+  def play(uri = nil)
+    if uri.nil?
       run 'play'
+    else
+      run "play track #{uri}"
     end
   end
 
@@ -32,7 +37,46 @@ module Spotify
     end
   end
 
+  [:artist, :album, :track].each do |sym|
+    define_method "search_#{sym}" do |name|
+      authenticate
+      RSpotify.const_get(sym.capitalize).search(name)
+    end
+  end
+
+  def albums_by_artist(id)
+    authenticate
+    RSpotify::Artist.find(id).albums
+  end
+
+  def tracks_in_album(album_id)
+    authenticate
+    RSpotify::Albums.find(id).tracks
+  end
+
+  # Might not be needed
+  def play_random_track_by_artist(id)
+    authenticate
+    play_random_track_on_album(albums_by_artist(id).sample.id)
+  end
+
+  # Might not be needed
+  def play_random_track_on_album(id)
+    authenticate
+    play tracks_in_album(id).sample.uri
+  end
+
   private
+
+  def config
+    @config ||= YAML.load_file('.spotifuby.yml')
+  end
+
+  def authenticate
+    return if @authenticated
+    RSpotify.authenticate(config[:client_id], config[:client_secret])
+    @authenticated = true
+  end
 
   def run(command)
     `osascript -e 'tell application \"Spotify\" to #{command}'`
